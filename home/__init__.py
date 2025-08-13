5aca80ed3715ae598161c6d6f10045b4d1f90763
Sanjana Hazare <hazaresanjana8@gmail.com>
2025-08-13 15:27:33 +0530

    27060

--- home/__init__.py
+++ home/__init__.py
@@ -34,7 +34,7 @@ header a {
     <a href="index.html">Home</a>
     </header>
 <div class="container">
-    <h1>404</h1>
+    <h1>Delicious Bites</h1>
     <p> Oops! The page you are looking for does not exist. </p>
 </div>
 </body>
